fb23515ab7d17cf65721b12bab26f056ae2b927c
Codkashh <as3733542@gmail.com>
2025-02-21 19:32:59 +0530

    coin details page

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Coin.css'
+import { useParams } from 'react-router-dom'
 
 const Coin = () => {
-  return (
-    <div>
-      <h2>Coin</h2>
-    </div>
-  )
+
+  const {coinId} = useParams();
+  const [coinData, setCoinData] = useState();
+  const {currency} = useContext(coinContext); 
+
+  const fetchCoinData = async () => {
+      const options = {
+          method: 'GET',
+          headers: {accept: 'application/json', 'x-cg-pro-api-key': 'CG-HLmMZF8HfHRoXAmLcVrQSmTe'}
+        };
+        
+        fetch(`https://pro-api.coingecko.com/api/v3/coins/markets?vs_currency=${coinId}`, options)
+          .then(response => response.json())
+          .then(response => setCoinData(response))
+          .catch(err => console.error(err));
+  }
+
+  useEffect(() => {
+    fetchCoinData();
+  },[currency])
+
+  if(coinData) {
+    return (
+      <div className='coin'>
+        <div className="coin-name">
+          <img src={coinData.image.large} alt="" />
+          <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
+        </div>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div className='spinner'>
+        <div className='spin'></div>
+      </div>
+    )
+  }
 }
 
 export default Coin
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react'
 import './Home.css' 
 import { CoinContext } from '../../context/CoinContext'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
 
@@ -52,7 +53,7 @@ const Home = () => {
         </div>
         {
           displayCoin.slice(0,10).map((item, index) => (
-            <div className="table-layout" key={index}>
+            <Link to={`/coin/${item.id}`} className="table-layout" key={index}>
               <p>{item.market_cap_rank}</p>
               <div>
                 <img src={item.image} alt="" />
@@ -61,7 +62,7 @@ const Home = () => {
               <p>{currency.symbol} {item.current_price.toLocaleString()}</p>
               <p className={price_change_percentage_24h > 0 ? "green" : "red"}>{Math.floor(item.price_change_percentage_24h*100)/100}</p>
               <p className='market-cap'>{currency.symbol} {item.market_cap.toLocaleString()}</p>
-            </div>
+            </Link>
           ))
         }
       </div>
